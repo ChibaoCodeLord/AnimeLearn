dcc0bc48deb41e18c49afa3a32b51141bd42888d
Le Duong Huy <leduonghuy.7.8.5@gmail.com>
2026-03-27 00:01:38 +0700

    chore: Thay đổi DB phù hợp với up video lên cloudinary

--- backend/src/models/Video.js
+++ backend/src/models/Video.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const videoSchema = new mongoose.Schema({
     uploader_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
-    source_url: { type: String, required: true },
-    source_type: { type: String, enum: ['youtube', 'local_upload'], required: true },
-    genre_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }], // Một video có nhiều thể loại
-    thumbnail: { type: String },
+    source_url: { type: String, required: true }, // Lưu link video từ Cloudinary trả về
+    cloudinary_public_id: { type: String }, // Khóa định danh trên Cloudinary để phục vụ việc xóa/sửa
+    source_type: { type: String, enum: ['youtube', 'cloudinary'], required: true }, 
+    genre_ids: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],
+    thumbnail: { type: String }, // Cũng có thể lưu link ảnh Cloudinary ở đây
     is_public: { type: Boolean, default: true }
 }, { timestamps: true });
 
