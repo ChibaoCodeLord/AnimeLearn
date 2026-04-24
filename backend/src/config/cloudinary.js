c5c921c1b9edda3dfd4ec7891f43db6252cd1e40
ChibaoCodeLord <102230059@sv1.dut.udn.vn>
2026-04-24 17:52:03 +0700

    cloudinary

---
+++ backend/src/config/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
+import multer from 'multer';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'animelearn_avatars',
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  },
+});
+
+export const uploadAvatar = multer({ storage: storage });
+export { cloudinary };
