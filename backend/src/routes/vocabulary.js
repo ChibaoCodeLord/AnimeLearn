c8b54e7f71e65d28c9e9c9b3708694a9dc54028b
Le Duong Huy <leduonghuy.7.8.5@gmail.com>
2026-05-03 16:53:48 +0700

    feat: thêm xóa từ vựng

--- backend/src/routes/vocabulary.js
+++ backend/src/routes/vocabulary.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/auth.js';
-import { getVocabulary, updateVocabulary } from '../controllers/vocabularyController.js';
+import { getVocabulary, updateVocabulary, deleteVocabulary } from '../controllers/vocabularyController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getVocabulary);
 router.patch('/:id', authMiddleware, updateVocabulary);
+router.delete('/:id', authMiddleware, deleteVocabulary);
+
 
 export default router;
